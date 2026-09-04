4f8217a88d75866b11c98e7397afe70616ce5415
Andrés <78932647+RooWiki@users.noreply.github.com>
2026-09-04 18:36:29 -0400

    ci: add Mesh Editor staging deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/mesheditor/',
+  build: {
+    outDir: 'dist/mesheditor',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
